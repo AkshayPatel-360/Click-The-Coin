48912c2d1b3a1c34010bc3dc5844b82d3fce0d4c
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-06-24 10:13:53 +1245

    Created coin Increment script
    Made two method for click increment and
    multiplayer Increment

---
+++ Assets/Resources/Scripts/CoinIncrement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinIncrement : MonoBehaviour
+{
+    [SerializeField]private Text ui;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        ui.text = "₿x" + GameManager.CoinMultiplayer + " : " + GameManager.BitCoin + " ₿";
+    }
+
+    public void ClickIncrement()
+    {
+        GameManager.BitCoin += GameManager.CoinMultiplayer;
+    }
+
+    public void MultiplayerIncrement(int num)
+    {
+        if (GameManager.BitCoin >= 10 && num == 1  )
+        {
+            GameManager.CoinMultiplayer += 2;
+            GameManager.BitCoin -= 10;
+        }
+
+        if (GameManager.BitCoin >= 100 && num == 2)
+        {
+            GameManager.CoinMultiplayer += 5;
+            GameManager.BitCoin -= 100;
+        }
+
+        if (GameManager.BitCoin >= 1000 && num == 3)
+        {
+            GameManager.CoinMultiplayer += 10;
+            GameManager.BitCoin -= 1000;
+        }
+
+        if (GameManager.BitCoin >= 10000 && num == 4)
+        {
+            GameManager.CoinMultiplayer += 50;
+            GameManager.BitCoin -= 10000;
+        }
+
+
+    }
+}
